80409b1fcc582d3ce9cbbe4af6c2592437165f45
Koen van der Drift <koenvanderdrift@gmail.com>
2019-07-31 17:08:31 -0400

    some more refactoring

--- Sources/Calculators/Hydropathy.swift
+++ Sources/Calculators/Hydropathy.swift
@@ -26,90 +26,63 @@ public class Hydropathy {
         
 // http://isoelectric.org/www_old/files/practise-isoelectric-point.html
 // https://stackoverflow.com/questions/30545518/how-to-count-occurrences-of-an-element-in-a-swift-array
-
-        let pKaDict = valuesDictionary(for: "pKa")
-        
         guard
-            let cTerminalpKa = pKaDict["CTerminal"],
-            let nTerminalpKa = pKaDict["NTerminal"],
-            let asparticAcidpKa = pKaDict["D"],
-            let glutamicAcidpKa = pKaDict["E"],
-            let cystinepKa = pKaDict["C"],
-            let tyrosinepKa = pKaDict["Y"],
-            let histidinepKa = pKaDict["H"],
-            let lysinepKa = pKaDict["K"],
-            let argininepKa = pKaDict["R"]
+            let pKaValues = hydropathyLibrary.first(where: { $0.name == "pKa" })?.values,
+            let cTerminalpKa = Double(pKaValues["CTerminal"]!),
+            let nTerminalpKa = Double(pKaValues["NTerminal"]!),
+            let asparticAcidpKa = Double(pKaValues["D"]!),
+            let glutamicAcidpKa = Double(pKaValues["E"]!),
+            let cystinepKa = Double(pKaValues["C"]!),
+            let tyrosinepKa = Double(pKaValues["Y"]!),
+            let histidinepKa = Double(pKaValues["H"]!),
+            let lysinepKa = Double(pKaValues["K"]!),
+            let argininepKa = Double(pKaValues["R"]!)
         else { return (0.0) }
 
-        let numberOfAsparticAcid = symbolSet.countFor("D")
-        let numberOfGlutamicAcid = symbolSet.countFor("E")
-        let numberOfCysteine = symbolSet.countFor("C")
-        let numberOfTyrosine = symbolSet.countFor("Y")
-        let numberOfHistidine = symbolSet.countFor("H")
-        let numberOfLysine = symbolSet.countFor("K")
-        let numberOfArginine = symbolSet.countFor("R")
+        let numberOfAsparticAcid = Double(symbolSet.countFor("D"))
+        let numberOfGlutamicAcid = Double(symbolSet.countFor("E"))
+        let numberOfCysteine = Double(symbolSet.countFor("C"))
+        let numberOfTyrosine = Double(symbolSet.countFor("Y"))
+        let numberOfHistidine = Double(symbolSet.countFor("H"))
+        let numberOfLysine = Double(symbolSet.countFor("K"))
+        let numberOfArginine = Double(symbolSet.countFor("R"))
 
         // starting point pI = 6.5 - theoretically it should be 7, but average protein pI is 6.5 so we increase the probability of finding the solution
         var pH = 6.5
         var minpH = 0.0
         var maxpH = 14.0
         let delta = 0.01
-        var temp = 0.0
-
-        var cTerminalCharge = 0.0
-        var asparticAcidCharge = 0.0
-        var glutamicAcidCharge = 0.0
-        var cysteineCharge = 0.0
-        var tyrosineCharge = 0.0
-
-        var nTerminalCharge = 0.0
-        var histidineCharge = 0.0
-        var lysineCharge = 0.0
-        var arginineCharge = 0.0
-
-        var neutralCharge = 0.0
 
         while pH - minpH > delta, maxpH - pH > delta {
-            cTerminalCharge = -(1 / (1 + pow(10, cTerminalpKa - pH)))
-            asparticAcidCharge = -(Double(numberOfAsparticAcid) / (1 + pow(10, asparticAcidpKa - pH)))
-            glutamicAcidCharge = -(Double(numberOfGlutamicAcid) / (1 + pow(10, glutamicAcidpKa - pH)))
-            cysteineCharge = -(Double(numberOfCysteine) / (1 + pow(10, cystinepKa - pH)))
-            tyrosineCharge = -(Double(numberOfTyrosine) / (1 + pow(10, tyrosinepKa - pH)))
-
-            nTerminalCharge = 1 / (1 + pow(10, pH - nTerminalpKa))
-            histidineCharge = Double(numberOfHistidine) / (1 + pow(10, pH - histidinepKa))
-            lysineCharge = Double(numberOfLysine) / (1 + pow(10, pH - lysinepKa))
-            arginineCharge = Double(numberOfArginine) / (1 + pow(10, pH - argininepKa))
-
-            neutralCharge = cTerminalCharge + asparticAcidCharge + glutamicAcidCharge + cysteineCharge + tyrosineCharge +
-                nTerminalCharge + histidineCharge + lysineCharge + arginineCharge
-
             if pH >= 14.0 {
                 break
             }
+            
+            let cTerminalCharge = -1 * (1 / (1 + pow(10, cTerminalpKa - pH)))
+            let asparticAcidCharge = -1 * (numberOfAsparticAcid / (1 + pow(10, asparticAcidpKa - pH)))
+            let glutamicAcidCharge = -1 * (numberOfGlutamicAcid / (1 + pow(10, glutamicAcidpKa - pH)))
+            let cysteineCharge = -1 * (numberOfCysteine / (1 + pow(10, cystinepKa - pH)))
+            let tyrosineCharge = -1 * (numberOfTyrosine / (1 + pow(10, tyrosinepKa - pH)))
+
+            let nTerminalCharge = 1 / (1 + pow(10, pH - nTerminalpKa))
+            let histidineCharge = numberOfHistidine / (1 + pow(10, pH - histidinepKa))
+            let lysineCharge = numberOfLysine / (1 + pow(10, pH - lysinepKa))
+            let arginineCharge = numberOfArginine / (1 + pow(10, pH - argininepKa))
+
+            let neutralCharge = cTerminalCharge + asparticAcidCharge + glutamicAcidCharge + cysteineCharge + tyrosineCharge +
+                nTerminalCharge + histidineCharge + lysineCharge + arginineCharge
+
             if neutralCharge < 0 { // we are out of range, thus the new pH value must be smaller
-                temp = pH
+                let temp = pH
                 pH = pH - ((pH - minpH) / 2)
                 maxpH = temp
             } else { // we used too small of a pH value, so we have to increase it
-                temp = pH
+                let temp = pH
                 pH = pH + ((maxpH - pH) / 2)
                 minpH = temp
             }
         }
 
         return pH
-    }
-
-    private func valuesDictionary(for name: String) -> [String: Double] {
-        var valuesDictionary = [String: Double]()
-
-        let flatMappedValues = (hydropathyLibrary.filter { $0.name == name }.map { $0.values }).flatMap { $0 }
-
-        flatMappedValues.forEach {
-            valuesDictionary[$0.0] = Double($0.1)
-        }
-
-        return valuesDictionary
     }
 }
