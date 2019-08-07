b3c7c3e91c81417a9120f392d39a16bcbc77ae7d
Koen van der Drift <koenvanderdrift@gmail.com>
2019-08-07 13:02:45 -0400

    Add Bond struct

--- Sources/Building Blocks/Modification.swift
+++ Sources/Building Blocks/Modification.swift
@@ -12,6 +12,32 @@ public class Modification: NSObject {
     }
 }
 
+public enum BondType {
+    case disulfide
+    case lactam
+    case other
+}
+
+public struct Bond {
+    public var from: Int
+    public var to: Int
+    public var type: BondType
+    
+    public init(from: Int, to: Int, type: BondType) {
+        self.from = from
+        self.to = to
+        self.type = type
+    }
+}
+
+extension Bond: Equatable {
+    public static func == (lhs: Bond, rhs: Bond) -> Bool {
+        // return true if to and from are the same or reversed
+        return (lhs.type == rhs.type) &&
+            ((lhs.from == rhs.from && lhs.to == rhs.to) || (lhs.from == rhs.to && lhs.to == rhs.from))
+    }
+}
+
 //extension Modification: Hashable {
 //    public static func == (lhs: Modification, rhs: Modification) -> Bool {
 //        return (lhs.group == rhs.group && lhs.location == rhs.location && lhs.site == rhs.site)
