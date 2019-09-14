93cd2436c46b8dba947a9d12a217bb63a4e36259
Koen van der Drift <koenvanderdrift@gmail.com>
2019-09-14 14:46:52 -0400

    Make Peptide its own class again; add precursor and ammonium ions; some renaming

--- Sources/Sequence/Peptide.swift
+++ Sources/Sequence/Peptide.swift
@@ -1,16 +1,49 @@
 import Foundation
 
-public typealias Peptide = Protein
+public class Peptide: BioSequence {
+    public override init(sequence: String, type: SequenceType = .protein, charge: Int = 0) {
+        super.init(sequence: sequence, type: type, charge: charge)
+    }
+}
 
 extension Peptide {
     public func fragment() -> [Fragment] {
-        return (nTerminalIons() + cTerminalIons())
-            //.sorted(by: { $0.massOverCharge().monoisotopicMass < $1.massOverCharge().monoisotopicMass })
+        return precursorIons() + immoniumIons() + nTerminalIons() + cTerminalIons()
     }
 
-    func nTerminalIons() -> [Fragment] { // b ions
-        var nTerminalFragmentIons: [Fragment] = []
+    func precursorIons() -> [Fragment] {
+        var fragments = [Fragment]()
+        
+        let fragment = Fragment(sequence: self.sequence, type: .protein, charge: self.charge, fragmentType: .precursor)
+        
+        fragments.append(fragment)
+        
+        if self.canLoseAmmonia() {
+            let fragment = Fragment(sequence: self.sequence, type: .protein, charge: self.charge, fragmentType: .precursor)
+            fragment.modifications = [Modification(group: FunctionalGroup(name: "lossOfAmmonia", masses: zeroMass - ammonia.masses), location: 0)]
+            fragments.append(fragment)
+        }
 
+        if self.canLoseWater() {
+            let fragment = Fragment(sequence: self.sequence, type: .protein, charge: self.charge, fragmentType: .precursor)
+            fragment.modifications = [Modification(group: FunctionalGroup(name: "lossOfWater", masses: zeroMass - water.masses), location: 0)]
+            fragments.append(fragment)
+        }
+
+        return fragments
+    }
+    
+    func immoniumIons() -> [Fragment] {
+        guard let symbols = self.symbolSet() as? Set<AminoAcid> else { return [] }
+        
+        return symbols.map { Fragment(sequence: $0.oneLetterCode, type: .protein, charge: $0.charge, fragmentType: .immonium) }
+    }
+
+    func nTerminalIons() -> [Fragment] { // b fragments
+        var fragments = [Fragment]()
+
+        guard self.charge > 0 else { return fragments }
+        
         for z in 1 ... min(2, self.charge) {
             for i in 2 ... sequence.count - 1 {
                 let index = sequence.index(sequence.startIndex, offsetBy: i) // let newStr = String(str[..<index])
@@ -19,34 +52,33 @@ extension Peptide {
                 fragment.charge = z
                 
                 if z == 1 {
-                    nTerminalFragmentIons.append(fragment)
+                    fragments.append(fragment)
                 } else {
                     if fragment.massOverCharge().monoisotopicMass > massOverCharge().monoisotopicMass {
-                        nTerminalFragmentIons.append(fragment)
+                        fragments.append(fragment)
                     }
                 }
             }
         }
 
-        return nTerminalFragmentIons
-            //.sorted(by: { $0.masses.monoisotopicMass < $1.masses.monoisotopicMass })
+        return fragments
     }
 
-    func cTerminalIons() -> [Fragment] {
-        // y ions
-        var cTerminalFragmentIons: [Fragment] = []
+    func cTerminalIons() -> [Fragment] { // y fragments
+        var fragments = [Fragment]()
 
+        guard self.charge > 0 else { return fragments }
+        
         for z in 1 ... min(2, self.charge) {
             for i in 1 ... sequence.count - 1 {
                 let index = sequence.index(sequence.endIndex, offsetBy: -i)
                 let fragment = Fragment(sequence: String(sequence[..<index]), type: self.type, charge: z, fragmentType: .cTerminal)
 
-                cTerminalFragmentIons.append(fragment)
+                fragments.append(fragment)
             }
         }
 
-        return cTerminalFragmentIons
-            //.sorted(by: { $0.masses.monoisotopicMass < $1.masses.monoisotopicMass })
+        return fragments
     }
 
     func canLoseWater() -> Bool {
