4472bf46c18b0f5fe9760eedb2415708c60a952e
Koen van der Drift <koenvanderdrift@gmail.com>
2019-12-15 13:08:25 -0500

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,28 @@
-// swift-tools-version:5.1
-/**
-*  BioSwift
-*  Copyright (c) Koen van der Drift 2019
-*  MIT license, see LICENSE file for details
-*/
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BioSwift",
     products: [
-        .library(name: "BioSwift", targets: ["BioSwift"])
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "BioSwift",
+            targets: ["BioSwift"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "BioSwift"),
-        .testTarget(name: "BioSwiftTests", dependencies: ["BioSwift"])
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "BioSwift",
+            dependencies: []),
+        .testTarget(
+            name: "BioSwiftTests",
+            dependencies: ["BioSwift"]),
     ]
 )
