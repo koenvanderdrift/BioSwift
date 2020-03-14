5a5d0c91432dfccdacdb81d682999396ddf5d4d8
Koen van der Drift <koenvanderdrift@gmail.com>
2020-03-14 16:51:21 -0400

    unmod parsing - part 2

--- Sources/Support/UnimodParser.swift
+++ Sources/Support/UnimodParser.swift
@@ -8,21 +8,93 @@
 
 import Foundation
 
-class UnimodParser: NSObject, XMLParserDelegate {
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+public var uniModifications = [Modification]()
+
+public class UnimodParser: NSObject, XMLParserDelegate {
+    var parser: XMLParser
     
+    var name = ""
+    var sites = [String]()
+    var delta = [String : Int]()
+    var reactions = [Reaction]()
+    
+    public init(xml: String) {
+        let xmlData = xml.data(using: String.Encoding.utf8)!
+        parser = XMLParser(data: xmlData)
+        
+        super.init()
+        
+        parser.delegate = self
     }
     
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-    
+    public func parseXML() {
+        parser.parse()
     }
     
-    func parser(_ parser: XMLParser, foundCharacters string: String) {
+    // MARK: XML Parser Delegate
     
+    public func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        if elementName == "umod:mod" {
+            if let title = attributeDict["title"] {
+                name = title
+            }
+        }
+        
+        else if elementName == "umod:specificity" {
+            if let site = attributeDict["site"], let classification = attributeDict["classification"] {
+                if classification.contains("Isotopic label") == false {
+                    sites.append(site)
+                }
+            }
+        }
+        
+        else if elementName == "umod:element" {
+            if let symbol = attributeDict["symbol"], let number = attributeDict["number"] {
+                delta[symbol] = Int(number)
+            }
+        }
     }
     
-    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
-    
+    public func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        if elementName == "umod:mod" {
+            
+            // todo -> move this code to Formula
+            let negativeElements = delta.filter({ $0.value < 0 })
+            if negativeElements.count > 0 {
+                var formula = ""
+                for (element, count) in negativeElements {
+                    formula.append(element)
+                    if count > 1 {
+                        formula.append(String(abs(count)))
+                    }
+                }
+                
+                let group = FunctionalGroup(name: "", formula: Formula(formula))
+                reactions.append(Reaction.remove(group))
+            }
+            
+            let postiveElements = delta.filter { $0.value > 0 }
+            if postiveElements.count > 0 {
+                var formula = ""
+                for (element, count) in postiveElements {
+                    formula.append(element)
+                    if count > 1 {
+                        formula.append(String(abs(count)))
+                    }
+                }
+                
+                let group = FunctionalGroup(name: "", formula: Formula(formula))
+                reactions.append(Reaction.add(group))
+            }
+            
+            let mod = Modification(name: name, reactions: reactions, sites: sites)
+            uniModifications.append(mod)
+            
+            name = ""
+            sites.removeAll()
+            delta.removeAll()
+            reactions.removeAll()
+        }
     }
 }
-
