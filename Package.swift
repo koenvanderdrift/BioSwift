9fac74bd177bc8810d98bb18b4fa5c12d8af91fa
Koen van der Drift <koenvanderdrift@gmail.com>
2020-08-07 15:44:42 -0400

    fix Resources…

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BioSwift",
-            dependencies: [])
+            dependencies: []),
         .testTarget(
             name: "BioSwiftTests",
             dependencies: ["BioSwift"]),
