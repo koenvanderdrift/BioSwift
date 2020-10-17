5720d93c45cfa4ade026f09148d05d12469a900e
Koen van der Drift <koenvanderdrift@gmail.com>
2020-10-17 13:08:51 -0400

    update SPM

--- Package.swift
+++ Package.swift
@@ -6,11 +6,15 @@ import PackageDescription
 let package = Package(
     name: "BioSwift",
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BioSwift",
-            targets: ["BioSwift"])
+            targets: ["BioSwift"]
+        ),
     ],
-     targets: [
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BioSwift",
             resources: [
@@ -21,10 +25,10 @@ let package = Package(
                .process("Resources/functionalgroups.json"),
                .process("Resources/hydropathy.json")
             ]
-        )
+        ),
         .testTarget(
             name: "BioSwiftTests",
             dependencies: ["BioSwift"]
-        )
+        ),
     ]
 )
