c080225d755e18548632391d67f65160aef62999
Koen van der Drift <koenvanderdrift@gmail.com>
2021-02-16 13:21:26 -0500

    cleanup decoding code

--- Sources/BioSwift/Data/JSON.swift
+++ Sources/BioSwift/Data/JSON.swift
@@ -9,26 +9,22 @@
 import Foundation
 
 public func loadJSONFromBundle<A: Decodable>(fileName: String) -> [A] {
-//    guard let bundle = Bundle(identifier: bioSwiftBundleIdentifier) else {
-//        fatalError("Unable to load bundle")
-//    }
-//
-//    guard let url = bundle.url(forResource: fileName, withExtension: "json") else {
-//        fatalError("Unable to find \(fileName).json")
-//    }
-
     guard let url = Bundle.module.url(forResource: fileName, withExtension: "json") else {
         fatalError("Unable to find unimod.xml")
     }
 
+    return loadJSONFromURL(url: url)
+}
+
+public func loadJSONFromURL<A: Decodable>(url: URL) -> [A] {
     guard let data = try? Data(contentsOf: url) else {
-        fatalError("Unable to load \(fileName).json")
+        fatalError("Unable to load \(url).json")
     }
 
     let decoder = JSONDecoder()
 
     guard let result = try? decoder.decode([A].self, from: data) else {
-        fatalError("Failed to decode \(fileName).json")
+        fatalError("Failed to decode \(url).json")
     }
 
     return result
--- Sources/BioSwift/Support/Bundle+Decoding.swift
+++ Sources/BioSwift/Support/Bundle+Decoding.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-public let bioSwiftBundleIdentifier = "BioSwift"
-
 public enum SharedResources {
     static public let aminoacidsURL = Bundle.module.url(forResource: "aminoacids", withExtension: "json")
     static public let elementsURL = Bundle.module.url(forResource: "elements", withExtension: "json")
@@ -21,35 +19,35 @@ public enum SharedResources {
 
 // via https://www.hackingwithswift.com/example-code/system/how-to-decode-json-from-your-app-bundle-the-easy-way
 
-extension Bundle {
-    func decode<T: Decodable>(_ type: T.Type,
-                              from file: String,
-                              dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
-                              keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
-        guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to locate \(file) in bundle.")
-        }
-
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load \(file) from bundle.")
-        }
-
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = dateDecodingStrategy
-        decoder.keyDecodingStrategy = keyDecodingStrategy
-
-        do {
-            return try decoder.decode(T.self, from: data)
-        } catch let DecodingError.keyNotFound(key, context) {
-            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' not found – \(context.debugDescription)")
-        } catch let DecodingError.typeMismatch(_, context) {
-            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
-        } catch let DecodingError.valueNotFound(type, context) {
-            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
-        } catch DecodingError.dataCorrupted(_) {
-            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON")
-        } catch {
-            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
-        }
-    }
-}
+//extension Bundle {
+//    func decode<T: Decodable>(_ type: T.Type,
+//                              from file: String,
+//                              dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
+//                              keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
+//        guard let url = self.url(forResource: file, withExtension: nil) else {
+//            fatalError("Failed to locate \(file) in bundle.")
+//        }
+//
+//        guard let data = try? Data(contentsOf: url) else {
+//            fatalError("Failed to load \(file) from bundle.")
+//        }
+//
+//        let decoder = JSONDecoder()
+//        decoder.dateDecodingStrategy = dateDecodingStrategy
+//        decoder.keyDecodingStrategy = keyDecodingStrategy
+//
+//        do {
+//            return try decoder.decode(T.self, from: data)
+//        } catch let DecodingError.keyNotFound(key, context) {
+//            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' not found – \(context.debugDescription)")
+//        } catch let DecodingError.typeMismatch(_, context) {
+//            fatalError("Failed to decode \(file) from bundle due to type mismatch – \(context.debugDescription)")
+//        } catch let DecodingError.valueNotFound(type, context) {
+//            fatalError("Failed to decode \(file) from bundle due to missing \(type) value – \(context.debugDescription)")
+//        } catch DecodingError.dataCorrupted(_) {
+//            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON")
+//        } catch {
+//            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+//        }
+//    }
+//}
