b5591c75419966fc466d3256004a5be28f812036
Koen van der Drift <koenvanderdrift@gmail.com>
2021-05-15 08:39:39 -0400

    Add BioMolecule struct

---
+++ Sources/BioSwift/Molecule/BioMolecule.swift
@@ -0,0 +1,14 @@
+//
+//  BioMolecule.swift
+//  
+//
+//  Created by Koen van der Drift on 5/9/21.
+//
+
+import Foundation
+
+public struct BioMolecule {
+    var name: String
+    var chains: [Chain]
+}
+
