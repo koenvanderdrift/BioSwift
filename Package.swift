655cbca297bab793745f3ad5269e2905b5880ce4
Koen van der Drift <koenvanderdrift@gmail.com>
2021-12-05 10:51:43 -0500

    And add the Swift Collections package

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "0.2.1")),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "0.0.3")),
     ],
     targets: [
         .target(
