3ea862ced99f1e3b7ed1402aee67e8aaef709835
Koen van der Drift <koenvanderdrift@gmail.com>
2022-01-29 12:45:53 -0500

    Rename Library -> DataLibrary; load unimod from DataLibrary

---
+++ Sources/BioSwift/Data/DataLibrary.swift
@@ -0,0 +1,57 @@
+//
+//  DataLibrary.swift
+//  BioSwift
+//
+//  Created by Koen van der Drift on 1/29/22.
+//
+
+import Foundation
+
+public var dataLibrary = DataLibrary()
+
+public var aminoAcidLibrary: [AminoAcid] = dataLibrary.library(with: .aminoAcid)
+public var elementLibrary: [ChemicalElement] = dataLibrary.library(with: .element)
+public var enzymeLibrary: [Enzyme] = dataLibrary.library(with: .enzyme)
+public var hydropathyLibrary: [Hydro] = dataLibrary.library(with: .hydropathy)
+public var modificationLibrary: [Modification] = dataLibrary.library(with: .modification)
+
+public enum LibraryType {
+    case aminoAcid
+    case element
+    case enzyme
+    case hydropathy
+    case modification
+}
+
+public struct DataLibrary {
+    private var unimodController = UnimodController()
+    
+    public func library<T: Decodable>(with type: LibraryType) -> [T] {
+        do {
+            switch type {
+//            case .aminoAcid: todo
+//            case .modification: todo
+            case .element:
+                return try parseJSONDataFromBundle(from: "elements")
+            case .enzyme:
+                return try parseJSONDataFromBundle(from: "enzymes")
+            case .hydropathy:
+                return try parseJSONDataFromBundle(from: "hydropathy")
+            default:
+                return []
+            }
+        } catch {
+            print("Error occurred \(error)")
+        }
+        
+        return []
+    }
+    
+    init() {
+        unimodController.loadUnimod { success in
+            guard success else {
+                fatalError("Could not load unimod")
+            }
+        }
+    }
+}
