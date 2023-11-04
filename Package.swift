942e563b62489352685544bbe6486bac9ba5984c
Koen van der Drift <koenvanderdrift@gmail.com>
2023-11-04 16:42:14 -0400

    Remove unused package dependencies

--- Package.swift
+++ Package.swift
@@ -13,8 +13,6 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
     ],
     targets: [
         .target(
