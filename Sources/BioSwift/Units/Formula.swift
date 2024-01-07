9c6352a7681ac15e9194ca1c4fac76c50ec66c72
Koen van der Drift <koenvanderdrift@gmail.com>
2024-01-07 10:04:02 -0500

    Added initial code for a chemical string using unicode for superscript and subscripts

--- Sources/BioSwift/Units/Formula.swift
+++ Sources/BioSwift/Units/Formula.swift
@@ -26,6 +26,10 @@ public class Formula {
     var description: String {
         formulaString
     }
+    
+    var chemicalString: String {
+        formulaString // TODO
+    }
 
     public func countedElements() -> NSCountedSet {
         NSCountedSet(array: elements)
@@ -200,6 +204,46 @@ public extension Formula {
         lhs = lhs + rhs
     }
 }
+
+public extension String {
+    func superScript() -> String {
+        var result = ""
+        for char in self {
+            if char == "+" { result.append("\u{207A}") }
+            else if char == "-" { result.append("\u{207B}") }
+            else if char == "1" { result.append("") }
+            else if char == "2" { result.append("\u{00B2}") }
+            else if char == "3" { result.append("\u{00B3}") }
+            else if char == "4" { result.append("\u{2074}") }
+            else if char == "5" { result.append("\u{2075}") }
+            else if char == "6" { result.append("\u{2076}") }
+            else if char == "7" { result.append("\u{2077}") }
+            else if char == "8" { result.append("\u{2078}") }
+            else if char == "9" { result.append("\u{2079}") }
+        }
+
+        return result
+    }
+
+    func subSript() -> String {
+        var result = ""
+        for char in self {
+            if char == "0" { result.append("\u{2080}") }
+            else if char == "1" { result.append("\u{2081}") }
+            else if char == "2" { result.append("\u{2082}") }
+            else if char == "3" { result.append("\u{2083}") }
+            else if char == "4" { result.append("\u{2084}") }
+            else if char == "5" { result.append("\u{2085}") }
+            else if char == "6" { result.append("\u{2086}") }
+            else if char == "7" { result.append("\u{2087}") }
+            else if char == "8" { result.append("\u{2088}") }
+            else if char == "9" { result.append("\u{2089}") }
+        }
+
+        return result
+    }
+}
+
 
 /*
  # Common chemical groups
