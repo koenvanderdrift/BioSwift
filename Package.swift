0af1b9a28568a38ac179671bf819631269f615e1
Koen van der Drift <koenvanderdrift@gmail.com>
2026-04-02 17:29:58 +0200

    Fix platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "BioSwift",
-    platforms: [.iOS(.v15), .macOS(.v10_13)],
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
