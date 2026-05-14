d89e3b9ac4d7eb57b61e9ef99a6ed14f688565c0
Koen van der Drift <koenvanderdrift@gmail.com>
2026-05-14 18:23:58 +0200

    Add id to Peptide

--- Sources/BioSwift/Molecule/Protein/Peptide.swift
+++ Sources/BioSwift/Molecule/Protein/Peptide.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public struct Peptide: Chain, Codable, Equatable {
+public struct Peptide: Chain, Codable, Equatable, Identifiable {
     public var name: String = ""
     public var residues: [AminoAcid] = []
     public var nTerminal: Modification = hydrogenModification
@@ -17,12 +17,15 @@ public struct Peptide: Chain, Codable, Equatable {
     public var adducts: [Adduct] = []
     public var range: ChainRange = zeroChainRange
     public var parentLength: Int = 0
+    public let id: UUID
     
     public init(sequence: String) {
+        self.id = UUID()
         residues = createResidues(from: sequence)
     }
 
     public init(residues: [AminoAcid]) {
+        self.id = UUID()
         self.residues = residues
     }
 
