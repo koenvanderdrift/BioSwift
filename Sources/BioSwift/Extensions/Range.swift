dcf3cd7f1d99112182ca06a8504e15fda071718b
Koen van der Drift <koenvanderdrift@gmail.com>
2026-07-14 18:56:10 +0200

    Undelete UIRange init from NSRange

--- Sources/BioSwift/Extensions/Range.swift
+++ Sources/BioSwift/Extensions/Range.swift
@@ -32,6 +32,21 @@ public struct UIRange: Equatable {
         self.value = value
     }
 
+    public init?(nsRange: NSRange) {
+        guard
+            nsRange.location != NSNotFound,
+            nsRange.location >= 0,
+            nsRange.length > 0
+        else {
+            return nil
+        }
+
+        let lowerBound = nsRange.location + 1
+        let upperBound = nsRange.location + nsRange.length
+
+        self.init(lowerBound ... upperBound)
+    }
+
     public var zeroBasedRange: Range<Int> {
         (value.lowerBound - 1) ..< value.upperBound
     }
