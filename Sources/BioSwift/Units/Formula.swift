9471cab447a9961ce2e5dccd6a979e554141abc7
Koen van der Drift <koenvanderdrift@gmail.com>
2026-09-03 12:25:48 +0200

    Remove more unicode

--- Sources/BioSwift/Units/Formula.swift
+++ Sources/BioSwift/Units/Formula.swift
@@ -307,68 +307,6 @@ extension Formula: MassRepresentable {
     }
 }
 
-//extension String {
-//    public func superScript() -> String {
-//        var result = ""
-//        for char in self {
-//            if char == "+" {
-//                result.append("\u{207A}")
-//            } else if char == "-" {
-//                result.append("\u{207B}")
-//            } else if char == "1" {
-//                result.append("")
-//            } else if char == "2" {
-//                result.append("\u{00B2}")
-//            } else if char == "3" {
-//                result.append("\u{00B3}")
-//            } else if char == "4" {
-//                result.append("\u{2074}")
-//            } else if char == "5" {
-//                result.append("\u{2075}")
-//            } else if char == "6" {
-//                result.append("\u{2076}")
-//            } else if char == "7" {
-//                result.append("\u{2077}")
-//            } else if char == "8" {
-//                result.append("\u{2078}")
-//            } else if char == "9" {
-//                result.append("\u{2079}")
-//            }
-//        }
-//
-//        return result
-//    }
-//
-//    public func subScript() -> String {
-//        var result = ""
-//        for char in self {
-//            if char == "0" {
-//                result.append("\u{2080}")
-//            } else if char == "1" {
-//                result.append("\u{2081}")
-//            } else if char == "2" {
-//                result.append("\u{2082}")
-//            } else if char == "3" {
-//                result.append("\u{2083}")
-//            } else if char == "4" {
-//                result.append("\u{2084}")
-//            } else if char == "5" {
-//                result.append("\u{2085}")
-//            } else if char == "6" {
-//                result.append("\u{2086}")
-//            } else if char == "7" {
-//                result.append("\u{2087}")
-//            } else if char == "8" {
-//                result.append("\u{2088}")
-//            } else if char == "9" {
-//                result.append("\u{2089}")
-//            }
-//        }
-//
-//        return result
-//    }
-//}
-
 /*
  # Common chemical groups
  GROUPS = {
